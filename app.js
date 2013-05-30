ae2d24f2fa82da35a1b7545e49eee607cbb5ea8d
aji <a.ji@daum.net>
2013-05-30 16:48:34 +0900

    redactor추가

--- app.js
+++ app.js
@@ -2,9 +2,20 @@
 $('#mynote')
 	.on("keyup",function(){
 		localStorage.setItem( "note", $('#mynote').val() );
-		// console.log(111)
 	})
 	.val( localStorage.getItem("note") );
+
+$('#mynote').redactor({
+	minHeight: 300,
+	keyupCallback: function(obj, event) {
+		// console.log(  )
+		var value = $('#mynote').getCode();
+		localStorage.setItem( "note", value );
+	}
+});
+
+
+
 
 
 // $('button.save').on("click",function(){
